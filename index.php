84cc479b5c6d393fa9a41d3b11e2234963a26703
AnhTu-Github <101334475+LeAnhTu-Github@users.noreply.github.com>
2023-11-14 00:27:15 +0700

    complete project

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body class=" bg-[#b3afaf] h-auto flex justify-center pt-2">
+    <main class="w-[630px] h-full bg-[#fe5604] rounded-xl ">
+        <div class=" w-full h-[445px] rounded-t-xl flex flex-col items-center">
+            <a href="#">
+                <img src="./assets/image/logoW.png" alt="error logo">
+            </a>
+            <div class="flex justify-between gap-1 w-full h-8 p-1">
+                <div class="bg-[#924C31] h-6 w-3/12 flex justify-center items-center">
+                    <a href="" class="text-white text-xs">Trang Chủ</a>
+                </div>
+                <div class="bg-[#924C31] h-6 w-3/12 flex justify-center items-center">
+                    <a href="" class="text-white text-xs">Giới Thiệu</a>
+                </div>
+                <div class="bg-[#924C31] h-6 w-3/12 flex justify-center items-center">
+                    <a href="" class="text-white text-xs">Diễn Đàn</a>
+                </div>
+                <div class="bg-[#924C31] h-6 w-3/12 flex justify-center items-center">
+                    <a href="" class="text-white text-xs">Fanpage</a>
+                </div>
+
+            </div>
+            <div class="w-full h-auto">
+                <img src="./assets/image/banner_2.png" alt="banner">
+            </div>
+        </div>
+        <div class="w-full h-21 px-14 flex justify-between gap-1 pt-4 bg-[#f2ad53]">
+            <a href="" class="w-1/6 h-15 flex flex-col  items-center">
+                <img src="./assets/image/jar.png" alt="jad">
+                <p class="text-[9px] text-[#D10932]">237 <span class="text-[#AD4105]">JAD</span></p>
+            </a>
+            <a href="" class="w-1/6 h-15 flex flex-col  items-center">
+                <img src="./assets/image/android.png" alt="jad">
+                <p class="text-[9px] text-[#D10932]">237 </p>
+                <span class="text-[#AD4105] text-[9px]">Hướng dẫn cài</span>
+            </a>
+            <a href="" class="w-1/6 h-15 flex flex-col  items-center">
+                <img src="./assets/image/play.png" alt="jad">
+                <p class="text-[9px] text-[#D10932]">237</p>
+            </a>
+            <a href="" class="w-1/6 h-15 flex flex-col  items-center">
+                <img src="./assets/image/pc.png" alt="jad">
+                <p class="text-[9px] text-[#D10932]">237</p>
+            </a>
+            <a href="" class="w-1/6 h-15 flex flex-col  items-center">
+                <img src="./assets/image/IpTestFlight.png" alt="jad">
+                <p class="text-[9px] text-[#D10932]">237</p>
+            </a>
+            <a href="" class="w-1/6 h-15 flex flex-col  items-center">
+                <img src="./assets/image/napngoc.png" alt="jad">
+                <span class="text-[#AD4105] text-[9px]">Báo lỗi SMS</span>
+                <span class="text-[#AD4105] text-[9px]">Báo lỗi thẻ</span>
+            </a>
+        </div>
+        <div class="mt-[4px] w-full h-auto p-1 bg-[#f2ad53] pb-8">
+            <h4 class="text-[#E73D00] font-bold">Giới Thiệu</h4>
+            <div class="w-full h-[1px] bg-black mb-4 mt-1"></div>
+            <p class="text-xs">
+                Ngọc Rồng Online là Trò Chơi Trực Tuyến với cốt truyện xoay quanh bộ truyện tranh 7 viên Ngọc Rồng.
+                Người chơi sẽ hóa thân thành một trong những anh hùng của 3 hành tinh: Trái Đất, Xayda, Namếc. Cùng
+                luyện tập, tăng cường sức mạnh và kỹ năng. Đoàn kết cùng chiến đấu chống lại các thế lực hung ác. Cùng
+                nhau tranh tài.
+            </p>
+            <p class="text-xs"> Đặc điểm nổi bật:</p>
+            <p class="text-xs">- Thể loại hành động, nhập vai. Trực tiếp điều khiển nhân vật hành động. Dễ chơi, dễ điều
+                khiển nhân vật. Đồ họa sắc nét. Có phiên bản đồ họa cao cho điện thoại mạnh và phiên bản pixel cho máy
+                cấu hình thấp.
+            </p>
+            <p class="text-xs"> - Cốt truyện bám sát nguyên tác. Người chơi sẽ gặp tất cả nhân vật từ Bunma, Quy lão
+                kame, Jacky-chun,
+                Tàu Pảy Pảy... cho đến Fide, Pic, Poc, Xên, Broly, đội Bojack.</p>
+            <p class="text-xs">- Đặc điểm nổi bật nhất: Tham gia đánh doanh trại độc nhãn. Tham gia đại hội võ thuật.
+                Tham gia săn lùng ngọc rồng để mang lại điều ước cho bản thân.</p>
+            <p class="text-xs"> Tương thích tất cả các dòng máy trên thị trường hiện nay: Máy tính PC, Điện thoại di
+                động Nokia Java, Android, iPhone, Windows Phone, và máy tính bảng Android, iPad.
+            </p>
+            <div class="w-full h-24 flex justify-center items-center gap-1 mt-4">
+                <img src="./assets/image/gif_maphongba.gif" alt="error" class="h-full w-1/6">
+                <img src="./assets/image/gif_gif_Saiyain.gif" alt="error" class="h-full w-1/6">
+                <img src="./assets/image/XD_Kame.gif" alt="error" class="h-full w-1/6">
+                <img src="./assets/image/TD_kaioshen.gif" alt="error" class="h-full w-1/6">
+            </div>
+            <div class="flex justify-center w-full mt-4">
+                <a href="" class="text-[#AD4105] text-xs">Xem thêm</a>
+            </div>
+        </div>
+        <div class="mt-1 w-full h-auto p-1 bg-[#f2ad53] pb-8">
+            <h4 class="text-[#E73D00] font-bold">Hướng Dẫn Tân Thủ</h4>
+            <div class="w-full h-[1px] bg-black mb-4 mt-1"></div>
+            <strong class="text-xs mb-4">1. Đăng ký tài khoản</strong>
+            <p class="text-xs mt-2">
+                Ngọc Rồng Online sử dụng Tài Khoản riêng, không chung với bất kỳ Trò Chơi nào khác.</p>
+            <p class="text-xs">Bạn có thể đăng ký tài khoản miễn phí ngay trong game, hoặc trên trang Diễn Đàn.</p>
+            <p class="text-xs">Khi đăng ký, bạn nên sử dụng đúng số điện thoại hoặc email thật của mình. Nếu sử dụng
+                thông tin sai,
+                người có số điện thoại hoặc email thật sẽ có thể lấy mật khẩu của bạn.</p>
+            <p class="text-xs"> Số điện thoại và email của bạn sẽ không hiện ra cho người khác thấy. Admin không bao giờ
+                hỏi mật khẩu
+                của bạn.</p>
+
+            <strong class="text-xs mb-4">2. Hướng dẫn điều khiển</strong>
+            <p class=" text-xs pt-2">Đối với máy bàn phím: Dùng phím mũi tên, phím số, để điều khiển nhân vật. Phím chọn
+                giữa
+                để tương tác.
+            </p>
+            <p class=" text-xs">Đối với máy cảm ứng: Dùng tay chạm vào màn hình cảm ứng để di chuyển. Chạm nhanh 2 lần
+                vào 1 đối tượng để
+                tương tác.</p>
+            <p class=" text-xs">Đối với PC: Dùng chuột, click chuột phải để di chuyển, click chuột trái để chọn, click
+                đôi vào đối tượng
+                để tương tác</p>
+            <strong class="text-xs">3. Một số thông tin căn bản</strong>
+            <p class="text-xs pt-2">
+                - Đậu thần dùng để tăng KI và HP ngay lập tức.
+                <br>
+                - Bạn chỉ mang theo người được 10 hạt đậu. Nếu muốn mang nhiều hơn, hãy xin từ bạn bè trong Bang.
+                <br>
+                - Tất cả các sách kỹ năng đều có thể học miễn phí tại Quy Lão Kame, khi bạn có đủ điểm tiềm năng.
+                <br>
+                - Bạn không thể bay, dùng kỹ năng, nếu hết KI.
+                <br>
+                - Tấn công quái vật cùng bạn bè trong Bang sẽ mang lại nhiều điểm tiềm năng hơn đánh một mình.
+                <br>
+                - Tập luyện với bạn bè tại khu vực thích hợp sẽ mang lại nhiều điểm tiềm năng hơn đánh quái vật.
+                <br>
+                - Khi được nâng cấp, đậu thần sẽ phục hồi nhiều HP và KI hơn.
+                <br>
+                - Vào trò chơi đều đặn mỗi ngày để nhận được Ngọc miễn phí.
+                <br>
+                - Đùi gà sẽ phục hồi 100% HP, KI. Cà chua phục hồi 100% KI. Cà rốt phục hồi 100% HP.
+                <br>
+                - Cây đậu thần kết một hạt sau một thời gian, cho dù bạn đang offline.
+                <br>
+                - Sau 3 ngày không tham gia trò chơi, bạn sẽ bị giảm sức mạnh do lười luyện tập.
+                <br>
+                - Bạn sẽ giảm thể lực khi đánh quái, nhưng sẽ tăng lại thể lực khi không đánh nữa.
+                <br>
+
+                Ngoài ra, bạn có thể tham khảo những
+                <span class="text-xs text-[#E73D00] ">thông tin sau</span> để có thể dễ
+                dàng tham gia trò
+                chơi hơn.
+            </p>
+        </div>
+        <div class="mt-[4px] w-full h-auto p-1 bg-[#f2ad53] pb-8">
+            <h4 class="text-[#E73D00] font-bold">Bạn nên tải phiên bản nào?</h4>
+            <div class="w-full h-[1px] bg-black mb-4 mt-1"></div>
+            <p class="text-xs">
+                Nếu bạn dùng điện thoại Nokia cũ, có bàn phím như Nokia 6300, Nokia E72, Nokia X2, Nokia C2, Hãy tải bản
+                JAVA
+                <br>
+                <br>
+                Nếu bạn dùng máy cảm ứng sử dụng Android như: Samsung Galaxy Y, HTC, LG, Sky, HKPhone. Hãy tải bản
+                Android APK hoặc Android Playstore đều được.
+                <br>
+                <br>
+                Nếu bạn dùng điện thoại cảm ứng của NOKIA Lumia, hoặc các máy HTC chạy Windows Phone, hãy tải bản cho
+                Windows Phone.
+                <br>
+                <br>
+                Nếu bạn dùng máy vi tính cá nhân, laptop chạy Windows XP - Windows 7, hãy tải bản PC.
+                <br>
+                <br>
+                Nếu bạn dùng iPhone, iPod, iPad, hãy tải bản iPhone Appstore. Nếu bạn biết chắc rằng máy mình đã
+                jailbreak, có cài AppSync hoặc AppstoreVN, hãy cài từ bản iPhone jailbreak để tốc độ nhanh hơn.
+            </p>
+        </div>
+        <div class="flex flex-col justify-center items-center text-[10px] pt-2">
+            <p>
+                Giấy phép thiết lập Mạng Xã Hội trên mạng số: 374/GP-BTTTT
+                <br>
+                do Bộ Thông Tin và Truyền Thông cấp ngày: 07/08/2015
+                <br>
+                Bản Quyền thuộc về TeaMobi - 2015 - Điều Khoản Sử Dụng
+            </p>
+            <select id="countries"
+                class=" w-1/3 h-5 bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-sm focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-3">
+                <option selected>Avatar</option>
+                <option value="US">United States</option>
+                <option value="CA">Canada</option>
+                <option value="FR">France</option>
+                <option value="DE">Germany</option>
+            </select>
+        </div>
+
+    </main>
+</body>
+
+</html>
